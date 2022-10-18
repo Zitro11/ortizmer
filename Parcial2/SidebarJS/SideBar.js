d7a46765bbd23b6c99cd3bce579f16cecdba4e5c
Zitro11 <61438031+Zitro11@users.noreply.github.com>
2022-10-17 23:09:00 -0500

    commit

---
+++ Parcial2/SidebarJS/SideBar.js
@@ -0,0 +1,10 @@
+window.onload=function ()
+{
+    document.getElementById("btnMenu").addEventListener("click",function() {
+        document.getElementById("barralat").classList
+        console.log("Click en el boton");
+
+        document.getElementByIdClassName("sidebar",)[0].classList.Toggle("escondido");
+
+    })
+}
