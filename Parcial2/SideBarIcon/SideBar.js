8231d0cc691416a6efe2783ff46cffc65a1163bf
Zitro11 <israelortiz2001@gmail.com>
2022-10-18 23:15:58 -0500

    commit

--- Parcial2/SideBarIcon/SideBar.js
+++
@@ -1,9 +0,0 @@
-window.onload=function()
-{
-    document.getElementById("btnMenu").addEventListener("click",function()
-    {
-        console.log("click en el boton, funcion de javascript realizada con exito");
-
-        document.getElementsByClassName("sidebar",)[0].classList.toggle("escondido");
-    })
-}
