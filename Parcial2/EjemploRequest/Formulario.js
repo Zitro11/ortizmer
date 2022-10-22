bd62f3ad95e7f17f0cc2a58562529fe8d45fa3e4
Zitro11 <israelortiz2001@gmail.com>
2022-10-21 19:22:32 -0500

    commit

--- Parcial2/EjemploRequest/Formulario.js
+++ Parcial2/EjemploRequest/Formulario.js
@@ -1,8 +1,14 @@
-$('#btnHoraAsincrono').click(function () {
+/*$('#btnHoraAsincrono').click(function () {
     var solicitud = new XMLHttpRequest();
     solicitud.onload = function () {
         document.getElementById("hora").value=solicitud.responseText;
     }
-});
+});*/
 
-$('#btnHoraSincrono').click
+//$('#btnHoraSincrono').click
+
+const btnRegistrar = document.querySelector('#BotonRegistro');
+
+btnRegistrar.addEventListener('click', function () {
+  console.log('clik en el boton registro')
+});
