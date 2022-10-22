7776fc436c460aa39ca16e8e77f67a306112e620
Zitro11 <israelortiz2001@gmail.com>
2022-10-22 15:07:18 -0500

    commit

--- Parcial2/EjemploRequest/Formulario.js
+++ Parcial2/EjemploRequest/Formulario.js
@@ -7,8 +7,23 @@
 
 //$('#btnHoraSincrono').click
 
-const btnRegistrar = document.querySelector('#BotonRegistro');
 
-btnRegistrar.addEventListener('click', function () {
-  console.log('clik en el boton registro')
-});
+/*var formulario = document.getElementById('formulario');
+
+formulario.addEventListener('submit', function(e){
+  e.preventDefault();
+  console.log("click");
+})*/ 
+
+document.querySelector('#BotonRegistro').addEventListener('click', Datos);
+
+function Datos()
+{
+  var solicitud = new XMLHttpRequest();
+
+   
+}
+
+/*document.getElementById("BotonRegistro").addEventListener('click', function () {
+  console.log("clik en el boton registro")
+});*/
