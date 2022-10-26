7bb377b3c5bc367b3bf086ea658a946d41676007
Zitro11 <israelortiz2001@gmail.com>
2022-10-25 21:09:32 -0500

    commit

--- Parcial2/XMLHttp/Formulario.js
+++ Parcial2/XMLHttp/Formulario.js
@@ -1,51 +1,25 @@
-/*$('#btnHoraAsincrono').click(function () {
-    var solicitud = new XMLHttpRequest();
-    solicitud.onload = function () {
-        document.getElementById("hora").value=solicitud.responseText;
-    }
-});*/
-
-//$('#btnHoraSincrono').click
-
-
-/*var formulario = document.getElementById('formulario');
-
-formulario.addEventListener('submit', function(e){
-  e.preventDefault();
-  console.log("click");
-})*/ 
-
 
 window.onload = function(){
   document.getElementById("btnAjax").addEventListener("click",pedirHeader);
   document.getElementById("btnReset").addEventListener("click",iniciarHeader);
-
-
 
 function pedirHeader()
 {
   var solicitud= new XMLHttpRequest();
   solicitud.onreadystatechange = function()
   {
-    if(solicitud.readyState == 4 && solicitud.status==200)
+    if(solicitud.readyState == 4 && solicitud.status == 200)
     {
-      document.getElementById("miDiv").innerHTML = solicitud.responseText;
+      document.getElementById("h").innerHTML = solicitud.responseText;
     }
   };
- solicitud.open("GET", "01_cargarArchivo.txt", true);
+ solicitud.open('GET', '01_cargarArchivo.txt', true);
  solicitud.send();
 }
 
 function iniciarHeader()
 {
-  document.getElementById("miDiv").innerHTML = "<h2>hola</h2>";
+  document.getElementById("h").innerHTML = "<h2>Mensaje</h2>";
 }
 
 }
-
-
-
-
-/*document.getElementById("BotonRegistro").addEventListener('click', function () {
-  console.log("clik en el boton registro")
-});*/
