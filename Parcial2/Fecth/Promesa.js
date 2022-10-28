f078b497cf2c4cb209dec31b8e80c66c35ab60e2
Zitro11 <israelortiz2001@gmail.com>
2022-10-27 19:55:39 -0500

    commit

---
+++ Parcial2/Fecth/Promesa.js
@@ -0,0 +1,7 @@
+document.getElementById('Boton').addEventListener('click',function(){HacerPeticion();});
+    function HacerPeticion()
+    {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then( respuesta => respuesta.json()) 
+        .then( datos => document.getElementById('Imagen').src = datos.message) 
+    } 
