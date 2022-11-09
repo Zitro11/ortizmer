181a6978fc4f0e564d298b4c4cd8e65d0d83bbae
Zitro11 <61438031+Zitro11@users.noreply.github.com>
2022-11-08 19:28:46 -0600

    commit

---
+++ Parcial3/ModuloCommonJS/App.js
@@ -0,0 +1,4 @@
+const modulo = require("./Modulo.js")
+
+console.log(modulo.cuadrado(2));
+
---
+++ Parcial3/ModuloCommonJS/Modulo.js
@@ -0,0 +1,14 @@
+const cuadrado = (valor) => { return valor * valor};
+
+function obtenerLongitud(cadena)
+{
+    return cadena.length;
+}
+
+
+//console.log (module);
+
+//console.log (cuadrado(2));
+
+exports.cuadrado = cuadrado;
+
