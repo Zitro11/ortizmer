da35981e4aa175f71828a153e576e5759ad3e437
Zitro11 <israelortiz2001@gmail.com>
2022-11-18 22:31:14 -0600

    commit

--- Parcial3/Formulario Bs Responsivo/App.php
+++ Parcial3/Formulario Bs Responsivo/App.php
@@ -4,12 +4,12 @@ $registro["Apellido"] = "Ortiz";
 $registro["Correo"] = "L19100@gmail.com";
 $registro["Genero"] = "Hombre";
 $registro["Telefono"] = "867 111 22 33";
-$registro["Nacimiento"] = "01/01/2001";
+$registro["Fecha"] = "2001-01-01";
 $registro["Origen"] = "Nuevo Laredo";
 $registro["Destino"] = "Monterrey";
-$registro["Aero"] = "Aeromexico";
+$registro["Aerolinea"] = "Aeromexico";
 $registro["Pasajeros"] = "1";
-$registro["Ida"] = "17/11/2022";
-$registro["Vuelta"] = "18/11/2022";
+$registro["Ida"] = "2022-11-17";
+$registro["Vuelta"] = "2022-11-18";
 echo json_encode($registro);
 ?>
