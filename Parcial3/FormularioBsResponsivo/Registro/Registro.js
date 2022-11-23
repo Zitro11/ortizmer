ff3289d6fe891ad1565bbbf8b35d64682d8c7665
Zitro11 <israelortiz2001@gmail.com>
2022-11-22 21:15:34 -0600

    commit

--- Parcial3/FormularioBsResponsivo/Registro/Registro.js
+++ Parcial3/FormularioBsResponsivo/Registro/Registro.js
@@ -1,4 +1,5 @@
 document.getElementById('btnRegistrarse').addEventListener('click',function(e){
     e.preventDefault();
-    window.location.href = "./FormularioBS copy.html";
+
+    window.location.href = "/Parcial3/FormularioBsResponsivo/iniciarSesion/Login.html";
 })
---
+++ Parcial3/FormularioBsResponsivo/iniciarSesion/Login.js
@@ -0,0 +1,4 @@
+document.getElementById('btnIniciar').addEventListener('click',function(e){
+    e.preventDefault();
+    window.location.href = "/Parcial3/FormularioBsResponsivo/FormularioBS copy.html";
+})
